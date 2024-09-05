3c29262cc914d9c1982de5378dc581f5d639e8f9
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-09-05 10:44:46 -0300

    feat: create time component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import Banner from "./components/Banner";
 import Formulario from "./components/Formulario/inde";
+import Time from './components/Time';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -15,6 +15,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador=> aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome='Programação'/>
+      <Time nome='Frontend'/>
     </div>
   );
 }
