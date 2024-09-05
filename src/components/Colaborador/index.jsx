fc30f3ca61dd90d9685dc1f1d760548685bf9425
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-09-05 11:37:08 -0300

    style: apply styling to collaborator cards

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+        <div className='cabecalho'>
+            <img src="https://github.com/mathiasfuhr.png" alt="Mathias Fuhr" />
+        </div>
+        <div className='rodape'>
+            <h4>Mathias Fuhr</h4>
+            <h5>Instrutor</h5>
+        </div>
+    </div>
+  )
+}
+
+export default Colaborador
